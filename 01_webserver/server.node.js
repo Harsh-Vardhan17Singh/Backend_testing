0a7d49b86fae52959a629dc7f3e38d301d3062f0
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2025-08-26 16:39:35 +0530

    Adding first draft in github

---
+++ 01_webserver/server.node.js
@@ -0,0 +1,28 @@
+const http = require('http')
+
+const hostname = 'localhost'
+const port = 3000
+
+const server = http.createServer((req,res) => {
+
+   if (req.url === '/') {
+     res.statusCode = 200
+    res.setHeader('Content-Type','text/plain')
+    res.end('Hello ice Tea')
+   }else if(req.url === '/ice-tea'){
+    res.statusCode = 200
+    res.setHeader('Content-Type','text/plain')
+    res.end('Thanks for ordering Iced tea, its really hot')
+   }else{
+    res.statusCode = 404
+    res.setHeader('Content-Type','text/plain')
+    res.end("404 Not Found")
+   }
+
+
+})
+
+server.listen(port,hostname, () =>{
+    console.log(`Server is Listening at the http://${hostname}:${port}`);
+    
+})
---
+++ 02_express.js/index.js
@@ -0,0 +1,70 @@
+import express from 'express'
+
+const app = express()
+
+const port = 3000
+app.use(express.json())
+
+let teaData = []
+let nextId = 1
+
+// Adding a new tea
+app.post('/teas',(req,res)=>{
+    
+    const {name,price}=req.body
+    const newTea = {id:nextId++,name,price}
+    teaData.push(newTea)
+    res.status(201).send(newTea)
+
+})
+
+//  get all the tea 
+app.get('/teas',(req,res) =>{
+    res.status(200).send(teaData)
+})
+
+// How to get a Single Tea using id
+// When you have URl you use req.params 
+//When you have inside the body then request it by req.body
+app.get ('/teas/:id',(req,res)=>{
+    const tea = teaData.find(t => t.id === parseInt(req.params.id))
+    if(!tea){
+        return res.status(404).send("Tea Not found")
+
+    }
+    res.status(200).send(tea)
+})
+
+//update tea
+// called business Logic
+app.put('/teas/:id',(req,res)=>{
+    const teaId = teaData.find(t => t.id === parseInt(req.params.id))
+
+    if(!teaId){
+        return res.status(404).send("Tea Not Found")
+    }
+    const {name,price} = req.price
+    tea.name = name,
+    tea.price = price,
+    res.status(200).send(teaId)
+
+})
+
+//delete Tea
+
+app.delete('/teas/:id',(req,res)=>{
+    const index = teaData.findIndex(t => t.id === parseInt(req.params.id))
+
+    if (index == -1) {
+        return res.status(404).send('tea Not Found')
+    }
+
+    teaData.splice(index,1)
+    return res.status(200).send('Selected Tea is Deleted')
+})
+
+
+app.listen(port,() =>{
+    console.log(`Server is running at port: ${port}...`);
+    
+})
